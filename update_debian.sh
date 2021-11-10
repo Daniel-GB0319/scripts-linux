17adbdb32ead8f4829eda6d845a69de2c6d1f86a
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2021-11-09 19:25:35 -0600

    Actualizado update_debian.sh a version 1.4

--- update_debian.sh
+++ update_debian.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Se alinearon mensajes de comfirmacion para cada comando finalizado con exito.
+
+clear
+echo ""
 echo "			***** Script para Actualizacion y Limpieza de la Distribucion *****"
-echo "		    		****** Autor: Daniel Gonzalez *******"
-echo "				 	      Ver. 1.3"
+echo "			    		****** Autor: Daniel Gonzalez *******"
+echo "					 	      Ver. 1.4"
 echo ""
 echo "Comandos a ejecutar:"
 echo "1) sudo apt update"
@@ -15,25 +20,25 @@ echo ""
 sleep 2s
 sudo apt update
 echo ""
-echo "!!! Ya estan los Repositorios Sincronizados !!!"
-echo "Ahora se actualizaran los Programas y/o Paqueterias en caso de haber nuevas versiones"
+echo "----------------------------- !!! Ya estan los Repositorios Sincronizados !!! ------------------------------"
+echo "            Ahora se actualizaran los Programas y/o Paqueterias en caso de haber nuevas versiones"
 echo ""
 sudo apt upgrade -y
 echo ""
-echo "!!! Ya estan Actualizados los Programas y/o Paqueterias !!!"
-echo "Ahora se actualizara la Distribucion, en caso de haber nuevas versiones"
+echo "----------------------- !!! Ya estan Actualizados los Programas y/o Paqueterias !!! ------------------------"
+echo "                   Ahora se actualizara la Distribucion, en caso de haber nuevas versiones"
 echo ""
 sudo apt full-upgrade -y 
 echo ""
-echo "!!! Ya esta la Distribucion Actualizada !!!"
-echo "Ahora se limpiaran los archivos descargados de actualizaciones que ya no se necesitan"
+echo "------------------------------- !!! Ya esta la Distribucion Actualizada !!! --------------------------------"
+echo "            Ahora se limpiaran los archivos descargados de actualizaciones que ya no se necesitan"
 echo ""
 sudo apt autoclean -y
 echo ""
-echo "!!! Limpieza Completada !!!"
-echo "Por ultimo se eliminaran paquetes que ya no son utiles"
+echo "--------------------------------------- !!! Limpieza Completada !!! ----------------------------------------"
+echo "                         Por ultimo se eliminaran paquetes que ya no son utiles"
 echo ""
 sudo apt autoremove -y
 echo ""
-echo "			******** Tarea de Actualizacion y Limpieza Terminada *******"
+echo "		            ******** Tarea de Actualizacion y Limpieza Terminada *******"
 echo ""
