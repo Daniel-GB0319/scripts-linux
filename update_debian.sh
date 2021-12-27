9421a58b8324250b856a0e89af784805e96d72bd
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2021-12-27 16:57:24 -0600

    Cambios realizados a mensajes de update-debian.sh

--- update_debian.sh
+++ update_debian.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Se alinearon mensajes de comfirmacion para cada comando finalizado con exito.
+# Se alinearon mensajes de comfirmacion para cada comando finalizado con exito. 27/Dic/2021
 
 clear
 echo ""
-echo "			***** Script para Actualizacion y Limpieza de la Distribucion *****"
-echo "			    		****** Autor: Daniel Gonzalez *******"
-echo "					 	      Ver. 1.4"
+echo "		***** Script para Actualizacion y Limpieza de la Distribucion *****"
+echo "		    		****** Autor: Daniel Gonzalez *******"
+echo "				 	     Ver. 1.4.1"
 echo ""
 echo "Comandos a ejecutar:"
 echo "1) sudo apt update"
@@ -15,30 +15,30 @@ echo "3) sudo apt full-upgrade -y"
 echo "4) sudo apt autoclean -y"
 echo "5) sudo apt autoremove -y"
 echo ""
-echo "Solo ingrese la contraseña para dar privilegios de SuperUser y espere a que el Script termine:"
+echo "Ingrese la contraseña para dar privilegios Root y espere a que el Script termine:"
 echo ""
 sleep 2s
 sudo apt update
 echo ""
-echo "----------------------------- !!! Ya estan los Repositorios Sincronizados !!! ------------------------------"
-echo "            Ahora se actualizaran los Programas y/o Paqueterias en caso de haber nuevas versiones"
+echo "------------------------ !!! Repositorios Sincronizados !!! --------------------------"
+echo "     Ahora se actualizaran los Programas/Paqueterias en caso de haber nuevas versiones"
 echo ""
 sudo apt upgrade -y
 echo ""
-echo "----------------------- !!! Ya estan Actualizados los Programas y/o Paqueterias !!! ------------------------"
-echo "                   Ahora se actualizara la Distribucion, en caso de haber nuevas versiones"
+echo "-------------------- !!! Programas/Paqueterias Actualizados !!! ----------------------"
+echo "        Ahora se actualizara la Distribucion, en caso de haber nuevas versiones"
 echo ""
 sudo apt full-upgrade -y 
 echo ""
-echo "------------------------------- !!! Ya esta la Distribucion Actualizada !!! --------------------------------"
-echo "            Ahora se limpiaran los archivos descargados de actualizaciones que ya no se necesitan"
+echo "------------------------ !!! Distribucion Actualizada !!! ----------------------------"
+echo "           Ahora se limpiaran los archivos de actualizacion innecesarios"
 echo ""
 sudo apt autoclean -y
 echo ""
-echo "--------------------------------------- !!! Limpieza Completada !!! ----------------------------------------"
-echo "                         Por ultimo se eliminaran paquetes que ya no son utiles"
+echo "--------------------------- !!! Limpieza Completada !!! ------------------------------"
+echo "                  Por ultimo se eliminaran paquetes innecesarios"
 echo ""
 sudo apt autoremove -y
 echo ""
-echo "		            ******** Tarea de Actualizacion y Limpieza Terminada *******"
+echo "		 ******** Tarea de Actualizacion y Limpieza Terminada *******"
 echo ""
