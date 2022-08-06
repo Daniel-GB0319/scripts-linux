bc8ae907cbdef8a64d3edf6bf0d4774f0a8f8c89
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2022-08-06 03:22:09 -0500

    Actualizado update_fedora.sh (version 1.1). Modificado la estructura de los mensajes hacia el usuario, ademas de cambiar los comandos que son utilizados en el script.

--- update_fedora.sh
+++ update_fedora.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
-# Primera version para RedHat y derivados.
+# Correccion y mejora de mensajes para visualizar en ventanas mas pequeñas. 6/Agosto/2022
 
 clear
 echo ""
-echo "			***** Script para Actualizacion y Limpieza de la Distribucion *****"
-echo "			    		****** Autor: Daniel Gonzalez *******"
-echo "					 	      Ver. 1.0"
+echo "*************************************************************"
+echo "** Script para Actualizacion y Limpieza de la Distribucion **"
+echo "*          ****** Autor: Daniel Gonzalez ******             *"
+echo "*                        Ver. 1.1                           *"
+echo "*************************************************************"
 echo ""
 echo "Comandos a ejecutar:"
 echo "1) sudo dnf -y upgrade"
-echo "2) sudo dnf -y distro-sync"
-echo "3) sudo dnf clean all"
-echo "5) sudo dnf -y autoremove"
+echo "2) sudo dnf -y dsync"
+echo "3) sudo dnf -y autoremove"
 echo ""
-echo "Solo ingrese la contraseña para dar privilegios de SuperUser y espere a que el Script termine:"
+echo "% Ingrese la contraseña y espere a que el Script finalice: %"
 echo ""
-sleep 2s
 sudo dnf -y upgrade
 echo ""
-echo "-------------------------- !!! Ya estan los Paquetes Sincronizados y Actualizados !!! ----------------------"
-echo "                    Ahora se actualizara la Distribucion, en caso de haber nuevas versiones"
+echo "#############################################"
+echo "#     !!! Paqueterias Actualizadas !!!      #"
+echo "# % Ahora se actualizara la Distribucion. % #"
+echo "#############################################"
 echo ""
-sudo dnf -y distro-sync
+sudo dnf -y dsync 
 echo ""
-echo "------------------------------- !!! Ya esta la Distribucion Actualizada !!! --------------------------------"
-echo "            Ahora se limpiaran los archivos descargados de actualizaciones que ya no se necesitan"
-echo ""
-sudo dnf clean all
-echo ""
-echo "------------------------------------ !!! Limpieza Completada !!! ----------------------------------------"
-echo "                         Por ultimo se eliminaran paquetes que ya no son utiles"
+echo "####################################################"
+echo "#         !!! Distribucion Actualizada !!!         #"
+echo "# % Ahora se limpiaran los paquetes innecesarios % #"
+echo "####################################################"
 echo ""
 sudo dnf -y autoremove
 echo ""
-echo "		            ******** Tarea de Actualizacion y Limpieza Terminada *******"
+echo "*******************************************************"
+echo "**** Script de Actualizacion y Limpieza Finalizada ****"
+echo "*******************************************************"
 echo ""
